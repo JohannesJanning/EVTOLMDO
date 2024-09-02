67ce5da5d23e72ee558c64722e71d4a48175aba2
JohannesJanning <johannes.janning23@imperial.ac.uk>
2024-09-02 11:13:55 +0100

    Update getFixedParameters1.m

--- Optimization/getFixedParameters1.m
+++ Optimization/getFixedParameters1.m
@@ -7,8 +7,8 @@ function params = getFixedParameters()
    
     % Model Flight Height Settings
     params.h_tofldg = 0; % [m], take-off and landing height AMSL, (above mean sea level)
-    params.h_hover = 30.5; % [m] Hover height AGL, (above ground level)
-    params.h_cruise = 304.7; % [m], Cruise height AGL (above ground level)
+    params.h_hover = 15.24; % [m] 30.5m = 100ft, 15.24m = 50ft, Hover height AGL, (above ground level)
+    params.h_cruise = 1219.2; % [m], 304.7m = 1000ft, Cruise height AGL (above ground level), 1219.2m = 4000ft
     
     % Atmospheric Model 
     rho_msl = 1.225; % [kg/m^3], air density at main sea level (MSL)
@@ -18,6 +18,7 @@ function params = getFixedParameters()
     params.rho_hover = rho_tofldg; 
     params.rho = rho_cruise; 
     params.rho_climb = rho_climb; 
+    params.rho_msl = rho_msl;
     
     % Aerodynamic Paramaters 
     params.alpha_deg_cruise = 3; % wing angle of attack in cruise [-]
@@ -27,8 +28,10 @@ function params = getFixedParameters()
     % Design: Number of Props in eVTOL Configuration 
     params.num_props_cruise = 1; % number of props in cruise (horizontal flight) configuration -
     params.num_props_hover = 8; % number of prop in hover (vertical flight) configuration -
-    params.d_tip = 0.4; % [m] % spacing between rotors and fuselage
+    params.d_tip = 0.025; % [m] % spacing between rotors and fuselage
     params.w_fuselage = 1.5; % [m] width of fuselage 
+    params.l_fus_m = 6; % assuming 9m for fuselage length (= aircraft length) [see Tecnam P2006T]
+    params.r_fus_m = 0.75; % assuming 1.5m for cabin width [see Tecnam P2006T]
 
     % Mass assumptions 
     params.M_pax = 82; % [kg], Mass per passenger 
@@ -38,15 +41,30 @@ function params = getFixedParameters()
     params.m_motor_initial = 500; % [kg], initial guess for motor mass
     params.m_bat_initial = 200; % [kg], initial guess for battery mass
     
+    % Battery assumptions
+    params.rho_bat = 400; % [Wh/kg] fixed assumption on battery density (400Wh/kg acc. Uber Eleveate requirement)
+    params.C_charge =1; % [1/h] C-rate (discharge rate of battery)
+
+
     % Economic assumptions
     params.N_s = 4; % Number of seats  
     params.P_s_empty = 1436.5; % [€/kg of M_empty], weight specific aircraft acquisition price  
-    params.N_wd = 260; % [days],  Number of working days per year 
+    params.N_wd = 260; % [days],  Number of working days per year, based on uber
     params.N_df = 6; % [-], Number of daily flights 
-    params.P_e = 0.09; % [€/kWh], energy price 
+    params.P_e = 0.096668; % [€/kWh], energy price 
     params.P_bat_s = 115; % [€/kWh], energy capacity specific battery replacement price 
     params.t_res = 60 * 30; % [seconds], 30min reserve for VFR acc. FAA 
     params.t_hover = 60; % [seconds], hover time in seconds
+    params.T_D = 8*60*60; % assuming a fixed operation window of 8hours daily, based on uber 
+    params.S_P = 45400; % [€] annual salary for one pilot, based on Uber 
+    params.N_AC = 1; % [-] number of aircraft controlled by one pilot (>1, bunker based operation)
+    params.U_pilot = 2000*60*60; %[sec] annual pilot utilization in seconds (2000hrs)
+    params.GWP_battery = 124.5, % [kg CO2e/kWh] LIB NMC battery life cycle global warming impact
+    params.GWP_energy = 0.37896; %[kg CO2/kWh] electrcity generation GWP for battery charging
+    params.LF = 0.67; % load factor, based on uber 
+    params.fare_km = 1.98; % euro per km per passenger, based on london taxi day fare
+
+
 
     % Efficiencies 
     params.eta_e = 0.9; % electric efficiency 
@@ -56,10 +74,10 @@ function params = getFixedParameters()
     params.eta_c = params.eta_p * params.eta_e; % horizontal flight efficiency 
 
     % Transportation Mode Comparison Figure of Merit Weighting 
-    params.time_weight = 0.25;
-    params.co2_weight = 0.25;
-    params.energy_weight = 0.25;
-    params.costs_weight = 0.25;
+    params.time_weight = 0.333;
+    params.co2_weight = 0.333;
+    params.energy_weight = 0;
+    params.costs_weight = 0.333;
 
 
 end
